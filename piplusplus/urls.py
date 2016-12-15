6063e84c91aaecbf2174ebb3c2eb1e2290e2fa65
angela1008 <angel.app2682@gmail.com>
2016-12-15 21:47:50 +0800

    setting admin interface creating users by admin written in db_user_detail

--- piplusplus/urls.py
+++ piplusplus/urls.py
@@ -14,9 +14,14 @@ Including another URLconf
     2. Import the include() function: from django.conf.urls import url, include
     3. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
-from django.conf.urls import url
+from django.conf.urls import patterns, url, include
 from django.contrib import admin
 
-urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-]
+## Bckended admin management
+from django.contrib import admin
+admin.autodiscover()
+
+## Nest-structure
+urlpatterns = patterns('',
+    url(r'^admin/', include(admin.site.urls)),
+)
