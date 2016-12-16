1b797060f82e0cd62f2f546939fdc8f4a2687104
angela1008 <angel.app2682@gmail.com>
2016-12-16 14:11:12 +0800

    Setting userextension model and register in admin.

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from api import models as apiModels
 
 # Register your models here.
+admin.site.register(apiModels.UserExtension)
--- api/models.py
+++ api/models.py
@@ -1,5 +1,16 @@
 from __future__ import unicode_literals
 
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+## User
+class UserExtension(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    gender = models.CharField(max_length=20, blank=True, null=True)
+    birth = models.CharField(max_length=50, blank=True, null=True)#
+    lacation = models.CharField(max_length=100, blank=True, null=True)
+    self_introduction = models.CharField(max_length=300, blank=True, null=True)#
+    interest = models.CharField(max_length=300, blank=True, null=True)#
+    created_at = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True, blank=True, null=True)
