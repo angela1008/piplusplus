6b9063f7d5effc257be036c61d931dd685d7b692
angela1008 <angel.app2682@gmail.com>
2016-12-18 15:46:57 +0800

    Change userextension model. Setting club model and register in admin.

--- api/admin.py
+++ api/admin.py
@@ -2,4 +2,15 @@ from django.contrib import admin
 from api import models as apiModels
 
 # Register your models here.
-admin.site.register(apiModels.UserExtension)
+class UserExtensionAdmin(admin.ModelAdmin):
+    list_display = ('user', 'gender', 'birth', 'interest', 'lacation', 'self_introduction', 'created_at', 'updated_at')
+
+class MembershipAdmin(admin.ModelAdmin):
+    list_display = ('member', 'club', 'date_joined', 'is_leader')
+
+class ClubAdmin(admin.ModelAdmin):
+    list_display = ('name', 'leader', 'club_introduction', 'created_at', 'updated_at')
+
+admin.site.register(apiModels.UserExtension, UserExtensionAdmin)
+admin.site.register(apiModels.Membership, MembershipAdmin)
+admin.site.register(apiModels.Club, ClubAdmin)
--- api/models.py
+++ api/models.py
@@ -8,9 +8,29 @@ from django.db import models
 class UserExtension(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     gender = models.CharField(max_length=20, blank=True, null=True)
-    birth = models.CharField(max_length=50, blank=True, null=True)#
-    lacation = models.CharField(max_length=100, blank=True, null=True)
-    self_introduction = models.CharField(max_length=300, blank=True, null=True)#
-    interest = models.CharField(max_length=300, blank=True, null=True)#
+    birth = models.DateField(blank=True, null=True)
+    lacation = models.CharField(max_length=50, blank=True, null=True)
+    self_introduction = models.TextField(max_length=254, blank=True, null=True)
+    interest = models.CharField(max_length=254, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True, blank=True, null=True)
     updated_at = models.DateTimeField(auto_now=True, blank=True, null=True)
+
+    
+## Club
+class Club(models.Model):
+    name = models.CharField(max_length=50)
+    leader = models.ForeignKey(User, on_delete=models.CASCADE)
+    club_introduction = models.TextField(max_length=254, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True, blank=True, null=True)
+    
+    ## Return club name instead of "Club Object"
+    def __unicode__(self):
+      return self.name
+    
+    
+class Membership(models.Model):
+    member = models.ForeignKey(User, on_delete=models.CASCADE)
+    club = models.ForeignKey(Club, on_delete=models.CASCADE)
+    date_joined = models.DateField(auto_now_add=True)
+    is_leader = models.BooleanField(default=False)
