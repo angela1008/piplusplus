3375651eb78b3f53a21c19cde3bde0b824e13451
vi83105 <vi83105@gmail.com>
2016-12-19 23:06:55 +0800

    add new event and progress model.also eventShip and progessShip

--- api/admin.py
+++ api/admin.py
@@ -3,14 +3,31 @@ from api import models as apiModels
 
 # Register your models here.
 class UserExtensionAdmin(admin.ModelAdmin):
-    list_display = ('user', 'gender', 'birth', 'interest', 'lacation', 'self_introduction', 'created_at', 'updated_at')
+    list_display = ('user', 'gender', 'birth', 'interest', 'location', 'self_introduction', 'created_at', 'updated_at')
 
 class MembershipAdmin(admin.ModelAdmin):
-    list_display = ('member', 'club', 'date_joined', 'is_leader')
+    list_display = ('member', 'club', 'date_joined', 'is_leader', 'created_at', 'updated_at')
 
 class ClubAdmin(admin.ModelAdmin):
     list_display = ('name', 'leader', 'club_introduction', 'created_at', 'updated_at')
+    
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('title', 'content', 'time', 'location', 'join_number', 'created_at', 'updated_at')
 
+class EventShipAdmin(admin.ModelAdmin):
+	list_display = ('member', 'event', 'club', 'created_at', 'updated_at')
+	
+class ProgressAdmin(admin.ModelAdmin):
+	 list_display = ('title', 'content', 'deadline', 'finished_number', 'created_at', 'updated_at')
+	 
+class ProgressShipAdmin(admin.ModelAdmin):
+	list_display = ('member', 'progress', 'club', 'is_finished', 'created_at', 'updated_at')
+    
+    
 admin.site.register(apiModels.UserExtension, UserExtensionAdmin)
 admin.site.register(apiModels.Membership, MembershipAdmin)
 admin.site.register(apiModels.Club, ClubAdmin)
+admin.site.register(apiModels.Event, EventAdmin)
+admin.site.register(apiModels.EventShip, EventShipAdmin)
+admin.site.register(apiModels.Progress, ProgressAdmin)
+admin.site.register(apiModels.ProgressShip, ProgressShipAdmin)
--- api/models.py
+++ api/models.py
@@ -9,14 +9,14 @@ class UserExtension(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     gender = models.CharField(max_length=20, blank=True, null=True)
     birth = models.DateField(blank=True, null=True)
-    lacation = models.CharField(max_length=50, blank=True, null=True)
+    location = models.CharField(max_length=50, blank=True, null=True)
     self_introduction = models.TextField(max_length=254, blank=True, null=True)
     interest = models.CharField(max_length=254, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True, blank=True, null=True)
     updated_at = models.DateTimeField(auto_now=True, blank=True, null=True)
 
     
-## Club
+# ## Club
 class Club(models.Model):
     name = models.CharField(max_length=50)
     leader = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -34,3 +34,49 @@ class Membership(models.Model):
     club = models.ForeignKey(Club, on_delete=models.CASCADE)
     date_joined = models.DateField(auto_now_add=True)
     is_leader = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True, blank=True, null=True)
+    
+
+## Event
+class Event(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField(max_length=254, blank=True, null=True)
+    time = models.DateField(blank=True, null=True)
+    location = models.CharField(max_length=100)
+    join_number=models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True, blank=True, null=True)
+	
+    def __unicode__(self):
+	    return self.title
+
+
+class EventShip(models.Model):
+    member = models.ForeignKey(User, on_delete=models.CASCADE)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    club = models.ForeignKey(Club, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True, blank=True, null=True)
+
+
+## Progress
+class Progress(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField(max_length=254,blank=True, null=True)
+    deadline = models.DateField()
+    finished_number = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True, blank=True, null=True)
+	
+    def __unicode__(self):
+	    return self.title
+
+
+class ProgressShip(models.Model):
+    member = models.ForeignKey(User, on_delete=models.CASCADE)
+    progress = models.ForeignKey(Progress, on_delete=models.CASCADE)
+    club = models.ForeignKey(Club, on_delete=models.CASCADE)
+    is_finished = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True, blank=True, null=True)
