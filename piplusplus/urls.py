f8ba31e906fa3399c1dfbf2d7cbfbb0b66a43ffd
angela1008 <angel.app2682@gmail.com>
2016-12-20 14:57:39 +0800

    Delete tempUI folder. Initial render page to .html.

--- piplusplus/urls.py
+++ piplusplus/urls.py
@@ -16,7 +16,14 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from app import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^sign-in/$', views.sign_in),
+    url(r'^sign-up/$', views.sign_up),
+    url(r'^index/$', views.index),
+    url(r'^front/$', views.front),
+    url(r'^profile/$', views.profile),
+    url(r'^group/$', views.group),
 ]
