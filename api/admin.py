f844dda3d2c4a50a4273cde3ef96c80add5554b5
angela1008 <angel.app2682@gmail.com>
2016-12-31 19:01:56 +0800

    Change name Club to Group. Change name Progress to Task.
    Add is_leader field to EventShip.
    Add is_heart, is_comment, is_share field to DiscussionShip.
    Add is_heart fild to CommentShip.
    Add UserInterest to db.
    
    Alter Event, Discussion, Comment, Tasks' member foreign key.

--- api/admin.py
+++ api/admin.py
@@ -3,47 +3,65 @@ from api import models as apiModels
 
 # Register your models here.
 class UserExtensionAdmin(admin.ModelAdmin):
-    list_display = ('id', 'user', 'gender', 'birth', 'interest', 'location', 'self_introduction', 'created_at', 'updated_at')
+    list_display = ('id', 'user', 'gender', 'birth', 'location', 'self_introduction', 'created_at', 'updated_at')
+    
+class UserInterestAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'name', 'created_at', 'updated_at')
+    
+
+class ClassificationAdmin(admin.ModelAdmin):
+    ist_display = ('id', 'name')
+    
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'leader', 'group_introduction', 'created_at', 'updated_at')
 
 class MembershipAdmin(admin.ModelAdmin):
-    list_display = ('id', 'member', 'club', 'date_joined', 'is_leader', 'created_at', 'updated_at')
+    list_display = ('id', 'member', 'group', 'date_joined', 'is_leader', 'created_at', 'updated_at')
 
-class ClubAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'leader', 'club_introduction', 'created_at', 'updated_at')
-    
+
 class EventAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'content', 'time', 'location', 'join_number', 'created_at', 'updated_at')
 
 class EventShipAdmin(admin.ModelAdmin):
-	list_display = ('id', 'member', 'event', 'club', 'created_at', 'updated_at')
+	list_display = ('id', 'member', 'event', 'group', 'is_leader', 'created_at', 'updated_at')
 	
-class ProgressAdmin(admin.ModelAdmin):
+	
+class TaskAdmin(admin.ModelAdmin):
 	 list_display = ('id', 'title', 'content', 'deadline', 'finished_number', 'created_at', 'updated_at')
 	 
-class ProgressShipAdmin(admin.ModelAdmin):
-	list_display = ('id', 'member', 'progress', 'club', 'is_finished', 'created_at', 'updated_at')
+class TaskShipAdmin(admin.ModelAdmin):
+	list_display = ('id', 'member', 'task', 'group', 'is_finished', 'created_at', 'updated_at')
+	
 	
 class DiscussionAdmin(admin.ModelAdmin):
 	list_display = ('id', 'title', 'content', 'heart_number', 'comment_number', 'share_number', 'created_at', 'updated_at')
 
 class DiscussionShipAdmin(admin.ModelAdmin):
-	list_display = ('id', 'member', 'discussion', 'club', 'created_at', 'updated_at')
+	list_display = ('id', 'member', 'discussion', 'group', 'is_heart', 'is_comment', 'is_share', 'created_at', 'updated_at')
+	
 	
 class CommentAdmin(admin.ModelAdmin):
     list_display = ('id', 'content', 'heart_number', 'created_at', 'updated_at')
     
 class CommentShipAdmin(admin.ModelAdmin):
-    list_display = ('id', 'member', 'comment', 'discussion', 'club', 'created_at', 'updated_at')
+    list_display = ('id', 'member', 'comment', 'discussion', 'is_heart', 'created_at', 'updated_at')
     
     
 admin.site.register(apiModels.UserExtension, UserExtensionAdmin)
+admin.site.register(apiModels.UserInterest, UserInterestAdmin)
+
+admin.site.register(apiModels.Classification, ClassificationAdmin)
+admin.site.register(apiModels.Group, GroupAdmin)
 admin.site.register(apiModels.Membership, MembershipAdmin)
-admin.site.register(apiModels.Club, ClubAdmin)
+
 admin.site.register(apiModels.Event, EventAdmin)
 admin.site.register(apiModels.EventShip, EventShipAdmin)
-admin.site.register(apiModels.Progress, ProgressAdmin)
-admin.site.register(apiModels.ProgressShip, ProgressShipAdmin)
+
+admin.site.register(apiModels.Task, TaskAdmin)
+admin.site.register(apiModels.TaskShip, TaskShipAdmin)
+
 admin.site.register(apiModels.Discussion, DiscussionAdmin)
 admin.site.register(apiModels.DiscussionShip, DiscussionShipAdmin)
+
 admin.site.register(apiModels.Comment, CommentAdmin)
 admin.site.register(apiModels.CommentShip, CommentShipAdmin)
