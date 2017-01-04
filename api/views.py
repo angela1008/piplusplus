f1ed40ba1494d2635956ef9194c895c427cfc6b0
angela1008 <angel.app2682@gmail.com>
2017-01-04 23:43:22 +0800

    Query user's group.(classification not yet !)
    Move front fromapp/views to api/views.
    Change name of  user_profile to own_profile

--- api/views.py
+++ api/views.py
@@ -1,14 +1,20 @@
 from django.shortcuts import render
 from django.shortcuts import render_to_response
 from django.contrib import auth
+from django.contrib.auth.models import User
 from django.http import HttpResponse
 from django.http import HttpResponseRedirect
-from django.contrib.auth.forms import UserCreationForm
+from django.db.models import Q
+from django.core.exceptions import ObjectDoesNotExist
+from django.core.urlresolvers import reverse
+
+from api import models
 
 # Create your views here.
 def login(request):
     if request.user.is_authenticated():
-        return HttpResponseRedirect('/front/')
+        # render to own front page
+        return HttpResponseRedirect('/front/?id=%d' %request.user.id)
         
     # If request.user is Anonymous User, we catch informations from request.POST.
     username = request.POST.get('username', '')
@@ -28,4 +34,60 @@ def login(request):
 def logout(request):
     auth.logout(request)
     return HttpResponseRedirect('/index/')
+
+# TODO not choose classification will error
+# TODO after create a new group render to own group page 
+# TODO classification(half)
+def front(request):
+    """
+    Add a new group with judging the error when filling the field.
+    (not filled in, existing group name)
+    """
+    classifications = models.Classification.objects.all()
+    
+    error = False
+    leader_error = False
+    name_error = False
+    classification_error = False
+    group = None
+    
+    if request.method == 'POST':
+        leader_id = request.POST.get('leader', None)
+        leader = User.objects.get(id=leader_id)
+        
+        name = request.POST.get('name')
+        # judge whether the group name exists
+        try:
+            models.Group.objects.get(name=name)
+        except ObjectDoesNotExist:
+            classification_id = request.POST.get('classification')
+            classification = models.Classification.objects.get(id=classification_id)
+            
+            # judge whether the field is filled in
+            if not name :
+                name_error = True
+            if not classification:
+                classification_error = True
+            if not leader_error and not name_error and not classification_error:
+                create_group = models.Group.objects.create(leader=leader, name=name, classification=classification)
+                models.Membership.objects.create(member=leader, group=create_group, is_leader=True)
+                group = models.Group.objects.get(id=create_group.id)
+            # if normal using then return directly,not got to error=True
+            return render_to_response('front.html',locals())
+            
+        # if "ObjectDoesExist" then set error and return to front page
+        error = True
+        return render_to_response('front.html',locals())
+    
+    # return groups
+    if request.method == 'GET':
+        user_id = request.GET.get('id', None)
+        
+        # get latest 8 groups
+        groups = models.Group.objects.order_by('-created_at')[:8]
+        # join
+        join_groups = models.Membership.objects.filter(Q(member=user_id) & Q(is_leader=False))
+        # leader
+        leader_groups = models.Group.objects.filter(leader=user_id)
+        return render_to_response('front.html',locals())
     
